9d9029f02af5d10d6e3c6083a6a9f3606944ea76
rkukin <romakukin@gmail.com>
2019-09-09 21:20:13 +0300

    task1 in progress

--- js/task1.js
+++ js/task1.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const categoriesCount = document.querySelectorAll("li.item");
+
+const categoriesTitles = document.querySelectorAll("li.item > h2");
+
+const subcategoriesCount = document.querySelectorAll("li.item > ul");
+
+console.log(`В списке ${categoriesCount.length} категории.`);
+
+for (let category of categoriesTitles) {
+  console.log(`Категория: ${category.innerText}`);
+  console.log(`Кол-во елементов: ${categoriesCount}`);
+}

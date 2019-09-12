2fb64490f508dbdabfc2f87337f3e83f4b417dbb
rkukin <romakukin@gmail.com>
2019-09-12 21:35:06 +0300

    task7

--- js/task7.js
+++ js/task7.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const slider = document.getElementById("font-size-slider");
+const text = document.getElementById("text");
+
+slider.addEventListener("input", e => {
+  text.style.fontSize = `${e.target.value}px`;
+});

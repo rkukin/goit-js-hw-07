c289bd54519131f270f6288442c6fe09054a8dff
rkukin <romakukin@gmail.com>
2019-09-12 22:20:18 +0300

    hw7 done

--- js/task1.js
+++ js/task1.js
@@ -6,5 +6,7 @@ console.log(`В списке ${categories.length} категории.`);
 
 for (let i = 0; i < categories.length; i++) {
   console.log(`Категория: ${categories[i].firstElementChild.innerText}`);
-  console.log(`Категория: ${categories[i].lastElementChild.childElementCount}`);
+  console.log(
+    `Количество элементов: ${categories[i].lastElementChild.childElementCount}`
+  );
 }

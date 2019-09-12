f34d526441c0bac39a988973658113e6279f31f9
rkukin <romakukin@gmail.com>
2019-09-12 18:38:29 +0300

    task2 done

--- js/task2.js
+++ js/task2.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
+];
+
+const createListItem = ingredients => {
+  const parentElem = document.querySelector("ul#ingredients");
+
+  for (const ingredient of ingredients) {
+    let listItem = document.createElement("li");
+    listItem.textContent = ingredient;
+    parentElem.append(listItem);
+  }
+};
+
+createListItem(ingredients);

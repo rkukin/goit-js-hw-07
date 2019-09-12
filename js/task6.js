1bd314b8954fa944c44ab693803877ba373bd7dc
rkukin <romakukin@gmail.com>
2019-09-12 21:19:03 +0300

    task6

--- js/task6.js
+++ js/task6.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const input = document.getElementById("validation-input");
+
+const validateInputLegth = e => {
+  if (e.target.value.length <= input.getAttribute("data-length")) {
+    input.classList.add("valid");
+    if (input.classList.contains("invalid")) {
+      input.classList.remove("invalid");
+    }
+  } else {
+    input.classList.add("invalid");
+    if (input.classList.contains("valid")) {
+      input.classList.remove("valid");
+    }
+  }
+};
+
+input.addEventListener("blur", validateInputLegth);

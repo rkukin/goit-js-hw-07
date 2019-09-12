eaea2cd89cc6ec5fd0658d86c18743c53089e964
rkukin <romakukin@gmail.com>
2019-09-12 20:06:12 +0300

    in progress

--- js/task4.js
+++ js/task4.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let counterValue = 0;
+
+const value = document.querySelector("#value");
+
+const increment = () => {
+  counterValue = counterValue + 1;
+  value.textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue = counterValue - 1;
+  value.textContent = counterValue;
+};
+
+document
+  .querySelector("[data-action='increment']")
+  .addEventListener("click", increment);
+
+document
+  .querySelector("[data-action='decrement']")
+  .addEventListener("click", decrement);
--- js/task5.js
+++ js/task5.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+
+const checkOutput = () => {
+  if (input.value === "") {
+    output.value = "незнакомец";
+  } else output.value = input.value;
+};
+
+input.addEventListener("input", console.log(input.value));
+console.log(input.value);

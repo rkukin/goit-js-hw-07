3c8bb21cecaeec3e06a478aa4ef54f37fac5bba5
rkukin <romakukin@gmail.com>
2019-09-12 22:16:17 +0300

    task8

--- js/task8.js
+++ js/task8.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const input = document.querySelector("#controls>input");
+const destroy = document.querySelector('[data-action="destroy"]');
+const render = document.querySelector('[data-action="render"]');
+const boxesContainer = document.querySelector("#boxes");
+
+function renderDivs() {
+  let divSize = 30;
+  for (let i = 0; i < input.value; i++) {
+    let div = document.createElement("div");
+    div.style.width = `${divSize}px`;
+    div.style.height = `${divSize}px`;
+    div.style.backgroundColor = `rgb(${Math.random() * 256}, ${Math.random() *
+      256}, ${Math.random() * 256})`;
+    boxesContainer.append(div);
+    divSize = divSize + 10;
+  }
+}
+
+render.addEventListener("click", renderDivs);
+
+destroy.addEventListener("click", () => {
+  while (boxesContainer.lastChild) {
+    boxesContainer.removeChild(boxesContainer.lastChild);
+  }
+});
